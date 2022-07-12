72cbf76ee5a5d561872f29bac6dada78a79a7e67
Vovnenko Oleksii <kespa71for@gmail.com>
2022-07-12 15:08:14 +0300

    Create: components -sidebar -pageWrapper -icon

---
+++ src/components/PageWrapper.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react';
+
+const PageWrapper:FC = () => {
+    return (
+        <div className='page-wrapper'>
+            PageWrapper
+        </div>
+    );
+};
+
+export default PageWrapper;
