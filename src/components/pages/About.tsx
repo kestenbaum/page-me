3817ffada11f17142f4ac19e39078b8a193ac60e
Vovnenko Oleksii <kespa71for@gmail.com>
2022-07-12 17:06:10 +0300

    create: components -about

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import avatar from './../../assets/images/me.png'
+
 
 const About = () => {
     return (
-        <div>
+        <div className='about'>
+            <div className='person-block'>
+                <div className='person-info'>
+                    <div className='person-avatar'>
+                        <img  className='avatar' src={avatar} alt='avatar'/>
+                    </div>
+                    <div className='person-document'>
+                        <span className='person-title'>Name: Oleksii</span>
+                        <span className='person-title'>Surname: Vovnenko</span>
+                    </div>
+                </div>
+            <div className='skills-block'>
+                <div className='person-title'>Skills:</div>
 
+            </div>
+            </div>
         </div>
     );
 };
