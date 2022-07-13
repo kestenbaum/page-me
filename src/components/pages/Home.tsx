2dcd74856da539f763d53aa48bbd4ad87990543b
Vovnenko Oleksii <kespa71for@gmail.com>
2022-07-13 18:49:45 +0300

    create: components -home

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {FC} from 'react';
 
-export const Home = () => {
+export const Home:FC = () => {
     return (
-        <div>
-            Home
+        <div className='home-page'>
+            <h1 className='home-title'>
+                hi everyone!!
+            </h1>
+            <p className='home-description'>
+                I'm an enthusiastic and detail-oriented Frontend Software Engineer seeking an entry-level position with Company to use my skills in coding, troubleshooting complex problems, and assisting in the timely completion of projects.
+            </p>
         </div>
     );
 };
