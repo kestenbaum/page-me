1c0a5b78aeb103d767e7a59ac0a6f5629f469178
Vovnenko Oleksii <kespa71for@gmail.com>
2022-07-16 15:36:40 +0300

    create: animation -contact description: Создал анимацию при наведение на контакты

--- src/components/pages/contact/Contact.tsx
+++ src/components/pages/contact/Contact.tsx
@@ -3,35 +3,45 @@ import Icon from "../../UI/icon/Icon";
 import {FaTelegramPlane} from 'react-icons/fa'
 import {BsPhone} from 'react-icons/bs'
 import {SiGmail} from 'react-icons/si'
+import {motion} from 'framer-motion'
 
 const Contact:FC = () => {
     return (
         <div className='contact-page'>
             <h2 className='person-title'>Contact</h2>
-            <div className='contact-block'>
+            <motion.div
+                className='contact-block'
+                whileHover={{scale: 1.1}}
+            >
                 <Icon
                     color={'white'}
                     size={'1.5em'}
                     children={<FaTelegramPlane />}
                 />
                 <span className='contact-link'>Telegram: @alexey_vvn</span>
-            </div>
-            <div className='contact-block'>
+            </motion.div>
+            <motion.div
+                whileHover={{scale: 1.1}}
+                className='contact-block'
+            >
                 <Icon
                     color={'white'}
                     size={'1.5em'}
                     children={<SiGmail />}
                 />
                 <a href="mailto:kespa71for@gmail.com" className='contact-link'>Gmail: kespa71for@gmail.com</a>
-            </div>
-            <div className='contact-block'>
+            </motion.div>
+            <motion.div
+                className='contact-block'
+                whileHover={{scale: 1.1}}
+            >
                 <Icon
                     color={'white'}
                     size={'1.5em'}
                     children={<BsPhone />}
                 />
                 <a href='tel:380666053013' className='contact-link'>Phone: +380666053013</a>
-            </div>
+            </motion.div>
         </div>
     );
 };
