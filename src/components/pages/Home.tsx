cb9fed431bffdefb2d8252d5b934d0ed465fbf41
Vovnenko Oleksii <kespa71for@gmail.com>
2022-07-16 16:28:23 +0300

    update: component -contact description: Декомпозировал код

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -8,7 +8,7 @@ export const Home:FC = () => {
                 className='home-title'
                 initial={{opacity: 0, x: -100}}
                 animate={{opacity: 1, x: 0}}
-                transition={{duration: 2}}
+                transition={{duration: 1}}
             >
                 hi everyone!!
             </motion.h1>
@@ -16,7 +16,7 @@ export const Home:FC = () => {
                 className='home-description'
                 initial={{opacity: 0, x: -100}}
                 animate={{opacity: 1, x: 0}}
-                transition={{duration: 2}}
+                transition={{duration: 1}}
             >
                 I'm an enthusiastic and detail-oriented Frontend Software Engineer seeking an entry-level position with Company to use my skills in coding, troubleshooting complex problems, and assisting in the timely completion of projects.
             </motion.p>
--- src/components/pages/contact/Contact.tsx
+++ src/components/pages/contact/Contact.tsx
@@ -1,47 +1,26 @@
-import React, {FC} from 'react';
-import Icon from "../../UI/icon/Icon";
+import React, {FC, useState} from 'react';
 import {FaTelegramPlane} from 'react-icons/fa'
 import {BsPhone} from 'react-icons/bs'
 import {SiGmail} from 'react-icons/si'
-import {motion} from 'framer-motion'
+import ContactBlock from "./ContactBlock";
 
 const Contact:FC = () => {
+
+    const [state, setState] = useState([
+        {id: 1, component: <FaTelegramPlane/>, contact: "Telegram: @alexey_vvn", href: "javascript:void(0)"},
+        {id: 2, component: <SiGmail/>, contact: "Gmail: kespa71for@gmail.com", href: "mailto:kespa71for@gmail.com"},
+        {id: 3, component: <BsPhone/>, contact: "Mobile Phone: +380666053013", href: 'tel:380666053013'},
+    ])
+
     return (
         <div className='contact-page'>
             <h2 className='person-title'>Contact</h2>
-            <motion.div
-                className='contact-block'
-                whileHover={{scale: 1.1}}
-            >
-                <Icon
-                    color={'white'}
-                    size={'1.5em'}
-                    children={<FaTelegramPlane />}
+            {state.map(item =>
+                <ContactBlock
+                    key={item.id}
+                    props={item}
                 />
-                <span className='contact-link'>Telegram: @alexey_vvn</span>
-            </motion.div>
-            <motion.div
-                whileHover={{scale: 1.1}}
-                className='contact-block'
-            >
-                <Icon
-                    color={'white'}
-                    size={'1.5em'}
-                    children={<SiGmail />}
-                />
-                <a href="mailto:kespa71for@gmail.com" className='contact-link'>Gmail: kespa71for@gmail.com</a>
-            </motion.div>
-            <motion.div
-                className='contact-block'
-                whileHover={{scale: 1.1}}
-            >
-                <Icon
-                    color={'white'}
-                    size={'1.5em'}
-                    children={<BsPhone />}
-                />
-                <a href='tel:380666053013' className='contact-link'>Phone: +380666053013</a>
-            </motion.div>
+            )}
         </div>
     );
 };
