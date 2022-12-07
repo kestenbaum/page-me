95c7e4a34e57a9caa5a9f01fb6bc139c63b0f882
oleksii vovnenko <kespa71for@gmail.com>
2022-12-07 16:29:40 +0300

    create firstScreen

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -1,9 +1,10 @@
 import React, {FC} from 'react';
-import {Work, About, Contact} from '../pages'
+import {Work, About, Contact, FirstScreen} from '../pages'
 
 export const PageWrapper:FC = () => {
     return (
         <div className='page-wrapper'>
+            <FirstScreen/>
             <About/>
             <Work/>
             <Contact/>
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,10 +1,12 @@
 import About from "./About";
 import Work from "./work/Work";
 import Contact from "./contact/Contact";
+import {FirstScreen} from "./FirstScreen/FirstScreen";
 
 
 export {
     About,
     Work,
-    Contact
+    Contact,
+    FirstScreen
 }
