26e8901805ba215a89520e57d15fc94419dea6c1
oleksii vovnenko <kespa71for@gmail.com>
2022-12-08 07:54:58 +0300

    delete sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React, {FC} from 'react';
 import './index.css';
 import {PageWrapper} from "./pages/PageWrapper/PageWrapper";
-import {Sidebar} from "./components/Sidebar/Sidebar";
-
 
 export const App:FC = () => {
     return (
         <div className='app'>
-            <Sidebar/>
             <div className="container">
                 <PageWrapper/>
             </div>
