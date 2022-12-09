4f5bf8c52249524f2cac6a805a51257176f29729
oleksii vovnenko <kespa71for@gmail.com>
2022-12-09 09:34:19 +0300

    create function for burger menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,44 +1,66 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import {Link} from "react-scroll";
 import cl from './Navbar.module.css';
 
 export const Navbar: FC = () => {
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+    const navigateClass = [cl.list]
+    if (isOpen) navigateClass.push(cl.active)
+
+    const toggleBurgerMenu = () => {
+        setIsOpen(!isOpen)
+        console.log(navigateClass)
+        console.log(isOpen)
+    }
+
     return (
-        <ul
-            className={cl.list}
-        >
-            <Link
-                to='home'
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-                className={cl.listItem}
-            ><span className={cl.listLink}>Home</span></Link>
-            <Link
-                to='skill'
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-                className={cl.listItem}
-            ><span className={cl.listLink}>Skills</span></Link>
-            <Link
-                to='work'
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-                className={cl.listItem}
-            ><span className={cl.listLink}>Works</span></Link>
-            <Link
-                to='contact'
-                smooth={true}
-                offset={-70}
-                duration={500}
-                className={cl.listItem}
-            ><span className={cl.listLink}>Contact</span></Link>
-        </ul>
+        <>
+            <ul
+                className={navigateClass.join(' ')}
+            >
+                <Link
+                    to='home'
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                    className={cl.listItem}
+                    onClick={() => setIsOpen(false)}
+                ><span className={cl.listLink}>Home</span></Link>
+                <Link
+                    to='skill'
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                    className={cl.listItem}
+                    onClick={() => setIsOpen(false)}
+                ><span className={cl.listLink}>Skills</span></Link>
+                <Link
+                    to='work'
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                    className={cl.listItem}
+                    onClick={() => setIsOpen(false)}
+                ><span className={cl.listLink}>Works</span></Link>
+                <Link
+                    to='contact'
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                    className={cl.listItem}
+                    onClick={() => setIsOpen(false)}
+                ><span className={cl.listLink}>Contact</span></Link>
+            </ul>
+            <button
+                className={cl.burger}
+                onClick={toggleBurgerMenu}
+            >
+                Burger Menu
+            </button>
+        </>
     );
 };
 
