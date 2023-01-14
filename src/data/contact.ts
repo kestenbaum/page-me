366481c3bd7825ef486be7f253efedaa12569ae7
oleksii vovnenko <kespa71for@gmail.com>
2023-01-14 12:30:15 +0300

    fix link contact block

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,5 +1,5 @@
 export const contactIcons = [
-    {id: 1, contact: "Telegram: @alexey_vvn", href: "https://t.me/oleksii_vvn"},
+    {id: 1, contact: "Telegram: Oleksii Vovnenko", href: "https://t.me/oleksii_vvn"},
     {id: 2, contact: "Gmail: kespa71for@gmail.com", href: "mailto:kespa71for@gmail.com"},
-    {id: 4, contact: "Github Page", href: 'https://github.com/kestenbaum'},
+    {id: 4, contact: "Github Page: Kestenbaum", href: 'https://github.com/kestenbaum'},
 ]
