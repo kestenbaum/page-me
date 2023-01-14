62c9da095e424c9080392da8e2d6862e7cfac49d
oleksii vovnenko <kespa71for@gmail.com>
2023-01-14 13:23:55 +0300

    fix grid system skills block

--- src/pages/Skills/Skills.tsx
+++ src/pages/Skills/Skills.tsx
@@ -3,19 +3,21 @@ import {dataIcons} from "../../data";
 import {SkillCard} from "../../components/SkillCard/SkillCard";
 import cl from './Skills.module.css';
 
-export const Skills:FC = () => {
+export const Skills: FC = () => {
     return (
         <div className={cl.wrapper}>
             <h2 className={cl.title} id='skill'>My skills</h2>
             <section className={cl.cardBlock}>
-                    {dataIcons.map(icon =>
+                {dataIcons.map(icon =>
+                    <div className={cl.col}>
                         <SkillCard
                             key={icon.id}
                             icon={icon.img}
                             title={icon.title}
                         />
-                    )}
-                </section>
+                    </div>
+                )}
+            </section>
         </div>
     );
 };
