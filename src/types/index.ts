79620fa432e0415508fb18f9e0d109dcc0012110
Oleksii Vovnenko <kespa71for@gmail.com>
2023-07-14 22:28:39 +0300

    update types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,37 +1,30 @@
 import React from "react";
-
 export interface IContactBlock {
     props: any
 }
-
 export interface ICardWork {
     props: IStateWork
 }
-
 export interface IStateWork {
     id: number
     title: string
     img: any
     link: string
 }
-
 export interface IButton extends
     React.DetailedHTMLProps<React.BaseHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
 {
     disabled?: boolean
 }
-
 export interface ISkillItem {
     skill: string
     component: any
 }
-
 export interface Icons {
     id: number,
     title: string,
     img: string
 }
-
 export interface SkillCardProps {
     title: string,
     icon: string | any
