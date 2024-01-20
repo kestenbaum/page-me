246d73bb7d73a569361685de263c16a574b19c1a
Oleksii Vovnenko <kespa71for@gmail.com>
2024-01-20 21:04:18 +0100

    refactor: link for project

--- src/data/work.ts
+++ src/data/work.ts
@@ -3,7 +3,6 @@ import it from "../assets/images/it.png";
 import work from "../assets/images/work.png";
 import technology from "../assets/images/technology.png";
 import aosta from "../assets/images/aosta.png";
-import repair from "../assets/images/repair.png";
 import cart from '../assets/images/cart.png';
 import drive from '../assets/images/drive.png';
 import cyber from '../assets/images/cyber.png';
@@ -14,37 +13,31 @@ export const workData = {
             id: 1,
             title: 'Airbag Layout',
             img: airbag,
-            link: 'https://kestenbaum.github.io/airbag-layout/'
+            link: 'https://kestenbaum.github.io/Airbag-Layout/'
         },
         {
             id: 2,
             title: 'IT Services',
             img: it,
-            link: 'https://kestenbaum.github.io/Portfolio/it/'
+            link: 'https://kestenbaum.github.io/IT-Blog/'
         },
         {
             id: 3,
             title: 'Work on yourself',
             img: work,
-            link: 'https://kestenbaum.github.io/working/'
+            link: 'https://kestenbaum.github.io/IT-Work/'
         },
         {
             id: 4,
             title: 'Technology',
             img: technology,
-            link: 'https://kestenbaum.github.io/technology/'
+            link: 'https://kestenbaum.github.io/Technology/'
         },
         {
             id: 5,
             title: 'Aosta',
             img: aosta,
-            link: 'https://kestenbaum.github.io/Portfolio/aosta/'
-        },
-        {
-            id: 6,
-            title: 'Repair',
-            img: repair,
-            link: 'https://kestenbaum.github.io/Portfolio/project/'
+            link: 'https://kestenbaum.github.io/Aosta/'
         }
     ],
     worksReact: [
