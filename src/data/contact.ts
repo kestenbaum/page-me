8fd7e25ee10f301e26c5709d65e9b70b4cdf2d65
Oleksii Vovnenko <kespa71for@gmail.com>
2024-03-06 01:58:49 +0100

    delete alter data

--- src/data/contact.ts
+++
@@ -1,5 +0,0 @@
-export const contactIcons = [
-    {id: 1, contact: "Telegram: Oleksii Vovnenko", href: "https://t.me/oleksii_vvn"},
-    {id: 2, contact: "Gmail: kespa71for@gmail.com", href: "mailto:kespa71for@gmail.com"},
-    {id: 4, contact: "Github Page: Kestenbaum", href: 'https://github.com/kestenbaum'},
-]
--- src/data/index.ts
+++
@@ -1,8 +0,0 @@
-
-import {contactIcons} from './contact'
-import {workData} from "./work";
-
-export {
-    contactIcons,
-    workData
-}
