8eb9c7c01c778d029b4d821ea608552450ef6b0f
Oleksii Vovnenko <kespa71for@gmail.com>
2024-03-06 01:59:30 +0100

    update page Works

--- src/pages/Work/Work.tsx
+++ src/pages/Work/Work.tsx
@@ -1,36 +1,15 @@
 import React, {FC} from 'react';
+import HtmlContent from "./HtmlContent/HtmlContent";
+import ReactContent from "./ReactContent/ReactContent";
 
-import {workData} from "../../data";
-import {CardWork} from "../../components/CardWork/CardWork";
 import cl from './Work.module.css';
 
 export const Work:FC = () => {
-    const getDataWork = workData.works;
-    const getDataReact = workData.worksReact;
-
     return (
         <div className={cl.wrapper} id='work'>
             <h2 className={cl.title}>Portfolio</h2>
-            <h3 className={cl.titleH2}>HTML/CSS/JS</h3>
-            <div className={cl.block}>
-                {
-                    getDataWork.length === 0
-                        ? <h2 className={cl.title}>Работы пока отсутствуют!</h2>
-                        : getDataWork.map(item =>
-                            <CardWork key={item.id} props={item}/>
-                        )
-                }
-            </div>
-            <h3 className={cl.titleH2}>React/Redux</h3>
-            <div className={cl.block}>
-                {
-                    getDataReact.length === 0
-                        ? <h2 className={cl.title}>Работы пока отсутствуют!</h2>
-                        : getDataReact.map(item =>
-                            <CardWork key={item.id} props={item}/>
-                        )
-                }
-            </div>
+            <HtmlContent/>
+            <ReactContent/>
         </div>
     );
 };
