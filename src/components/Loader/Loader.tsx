fe5d82d732ff4cca942f265478a71086f55ee8a0
Oleksii Vovnenko <kespa71for@gmail.com>
2024-03-08 16:59:38 +0100

    create component Loader

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,13 @@
+import React, {FC} from 'react';
+
+import style from './Loader.module.css';
+const Loader:FC = () => {
+    return (
+        <div className={style.wrapper}>
+            <div className={style.logo}></div>
+            <div className={style.text}>Loading...</div>
+        </div>
+    );
+};
+
+export default Loader;
