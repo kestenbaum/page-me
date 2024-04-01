f3004a9a96cf6a4e03216c94c93b067115a10e4d
Oleksii Vovnenko <kespa71for@gmail.com>
2024-04-01 16:08:18 +0200

    update component: update request

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,22 +1,28 @@
 import React, { FC } from 'react';
-import { useQuery } from 'react-query';
-import { contactServices } from '../../services/contact.services';
 
+import { useQuery } from 'react-query';
+import { getContacts } from '../../api/requests/contacts';
 import { ContactBlock } from './ContactBlock/ContactBlock';
 
+import Loader from '../UI/Loader/Loader';
 import cl from './Contact.module.css';
-import Loader from '../Loader/Loader';
 
 export const Contact: FC = () => {
   const { data, isLoading } = useQuery({
-    queryKey: ['contact'],
-    queryFn: () => contactServices.getContact()
+    queryKey: ['contacts'],
+    queryFn: () => getContacts().then((responce) => responce?.data)
   });
 
   return (
     <section className={cl.wrapper} id='contact'>
       <h2 className={cl.title}>Contact</h2>
-      {isLoading ? <Loader /> : data?.map((item) => <ContactBlock key={item._id} props={item} />)}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        data?.data.map((item) => (
+          <ContactBlock key={item._id} title={item.title} value={item.value} />
+        ))
+      )}
     </section>
   );
 };
