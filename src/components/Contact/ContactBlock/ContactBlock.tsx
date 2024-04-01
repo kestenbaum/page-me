b8fcbe25d71920cd71c7165eb940ce9b5e2b8031
Oleksii Vovnenko <kespa71for@gmail.com>
2024-04-01 16:08:34 +0200

    update component: update props

--- src/components/Contact/ContactBlock/ContactBlock.tsx
+++ src/components/Contact/ContactBlock/ContactBlock.tsx
@@ -1,13 +1,13 @@
 import React, { FC } from 'react';
 import { motion } from 'framer-motion';
-import { IContactBlock } from '../../../types';
+
 import cl from './ContactBlock.module.css';
 
-export const ContactBlock: FC<IContactBlock> = ({ props }) => {
+export const ContactBlock: FC<Contact> = ({ value, title }) => {
   return (
     <motion.div className={cl.wrapper} whileHover={{ scale: 1.03 }}>
-      <a href={props.value} className={cl.title} target='_blank'>
-        {props.title}
+      <a href={value} className={cl.title} target='_blank'>
+        {title}
       </a>
     </motion.div>
   );
