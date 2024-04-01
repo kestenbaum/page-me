135c195a00f9a0e580263037d9b6852dff71bec6
Oleksii Vovnenko <kespa71for@gmail.com>
2024-04-01 16:04:05 +0200

    update imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import { Header } from './components/Header/Header';
-import './index.css';
 import { PageWrapper } from './components/PageWrapper/PageWrapper';
+import './index.css';
 
 export const App: FC = () => {
   return (
