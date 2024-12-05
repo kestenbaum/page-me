e4beeecdcfaa79297288b0cd0bfa292b0918233c
Oleksii Vovnenko <kespa71for@gmail.com>
2024-12-05 03:07:44 +0100

    update Intro: add error hendler

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -6,7 +6,9 @@ const Intro = () => {
         <div className={style.wrapper} id='home'>
             <h1 className={style.title}>Hi everyone,</h1>
             <div className={style.description}>
-                {INTRO_DATE.map(((element, idx) => <p key={idx}>{element}</p>))}
+                {!Array.isArray(INTRO_DATE)
+                    ? []
+                    : INTRO_DATE.map(((element, idx) => <p key={idx}>{element}</p>))}
             </div>
             <h2>Thank You.</h2>
         </div>
