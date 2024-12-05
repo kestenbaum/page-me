58b65201034445161042319d8ea974300e620406
Oleksii Vovnenko <kespa71for@gmail.com>
2024-12-05 04:28:22 +0100

    update App:

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosRequestConfig, AxiosError } from 'axios';
+import axios from 'axios';
 
 export const api = axios.create({
   baseURL: 'https://kestenbaum-page-me-api.onrender.com/api',
