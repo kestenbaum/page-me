0c07918ad7939bf0adfc79d50a00da0be230de07
Oleksii Vovnenko <kespa71for@gmail.com>
2024-12-05 04:38:34 +0100

    update Intro: refactor code

--- @types/api.d.ts
+++ @types/api.d.ts
@@ -4,11 +4,6 @@ interface Contact {
   value: string;
 }
 
-type GetContacts = {
-  data: Contact[];
-  status: number;
-};
-
 interface Card {
   _id?: string;
   title: string;
@@ -22,17 +17,4 @@ interface Skill {
   img: string;
 }
 
-type GetSkills = {
-  data: Skill[];
-  status: number;
-};
 
-interface HeaderMenuProps {
-  id: number,
-  to: string,
-  link: string
-}
-
-interface LogoProps {
-  children: string
-}
--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,7 +1,18 @@
-import {INTRO_DATE} from "../../constants";
+import { FC } from "react";
 import style from './Intro.module.css';
 
-const Intro = () => {
+const INTRO_DATE: string[] = [
+    " my name's Oleksii. I'm from Ukraine and have been a web developer for the last 2 years. I\n" +
+    "                    believe in strong dedication in each project of my clients and client's satisfaction is the\n" +
+    "                    first concern in my career.",
+    " I'm adequate in the prominent skills HTML, CSS, Javascript, different web hosting sectors\n" +
+    "                    and so on. I can solve problems analytically and can face any complex situation with a\n" +
+    "                    molive of fixing it with total vividness. Also, I handle customers with a friend like manner\n" +
+    "                    keeping myself within professionalism.",
+    "You can pick me with an assurance of honest and best services."
+]
+
+const Intro: FC = () => {
     return (
         <div className={style.wrapper} id='home'>
             <h1 className={style.title}>Hi everyone,</h1>
