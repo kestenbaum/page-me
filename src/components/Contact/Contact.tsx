28a039f3ab78099da2fc2c15c4badc6af4fe8067
Oleksii Vovnenko <kespa71for@gmail.com>
2024-12-05 03:05:57 +0100

    update contact: add hook and error hendler

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,32 +1,37 @@
-import {useQuery} from 'react-query';
-import {getContacts} from '../../api/requests/contacts';
+import React, {FC} from "react";
+import useFetchContact from "../../hooks/useFetchContact";
 import Loader from '../UI/Loader/Loader';
+
 import style from './Contact.module.css';
 
 
-export const Contact = () => {
-    const {data, isLoading} = useQuery({
-        queryKey: ['contacts'],
-        queryFn: () => getContacts().then((responce) => responce?.data)
-    });
+export const Contact: FC = () => {
+    const { data, isLoading, isError, error } = useFetchContact();
 
     return (
         <section className={style.wrapper} id='contact'>
-            <h2 className={style.title}>Contact</h2>
-            {isLoading ? (
-                <Loader/>
-            ) : (
-                data?.data.length && data?.data.map((item, idx) => (
-                    <div
-                        key={idx}
-                        className={style.wrapper}
-                    >
-                        <a href={item.value} className={style.title} target='_blank'>
-                            {item.title}
-                        </a>
-                    </div>
-                ))
-            )}
+            <>
+                <h2 className={style.title}>Contact</h2>
+                {isLoading ? (
+                    <Loader/>
+                ) : (
+                    !Array.isArray(data) ? [] : data.map((item: Contact, idx: number) => (
+                        <div
+                            key={idx}
+                            className={style.wrapper}
+                        >
+                            <a href={item.value} className={style.title} target='_blank'>
+                                {item.title}
+                            </a>
+                        </div>
+                    ))
+                )}
+                {isError && error && (
+                    <span className={style.error}>
+                        {error instanceof Error ? error.message : "An unknown error occurred."}
+                    </span>
+                )}
+            </>
         </section>
     );
 };
