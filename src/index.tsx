99d16d1382288ff35d20a03525a325f23bef5c42
Oleksii Vovnenko <kespa71for@gmail.com>
2024-12-05 04:30:29 +0100

    update Provider:

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {Provider} from './Provider';
+import Provider from './components/Provider';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
