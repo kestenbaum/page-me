c77739741677230641daae62fd9f764e2faa8d29
Oleksii <kespa71for@gmail.com>
2026-05-03 17:53:56 +0200

    fix: disable type checking and add CI flag for vercel build

--- craco.config.js
+++ craco.config.js
@@ -8,5 +8,18 @@ module.exports = {
         alias: {
             '@': path.resolve(__dirname, 'src'),
         },
+        configure: (webpackConfig) => {
+            if (webpackConfig.optimization) {
+                webpackConfig.optimization.minimize = process.env.NODE_ENV === 'production';
+            }
+            const miniCssPlugin = webpackConfig.plugins.find(
+                (plugin) => plugin.constructor.name === 'MiniCssExtractPlugin'
+            );
+            if (miniCssPlugin) {
+                miniCssPlugin.options.ignoreOrder = true;
+            }
+            
+            return webpackConfig;
+        },
     },
 };
