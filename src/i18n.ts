c2ddd9ac7dbfae049d804dddb3b731c76c5d8cf4
Oleksii <kespa71for@gmail.com>
2026-05-21 09:26:54 +0200

    update: skills

--- src/i18n.ts
+++ src/i18n.ts
@@ -21,8 +21,8 @@ i18n
             title: 'Hi everyone ,',
             thanks: 'Thank You.',
             intro: [
-              'My name is Oleksii. I’m a Web Developer from Ukraine with over 2 years of experience. I believe in deep dedication to every project, where client satisfaction is the primary goal of my career.',
-              'I specialize in HTML, CSS, and JavaScript, with a solid understanding of web hosting and infrastructure. I approach complex challenges with an analytical mindset, ensuring clear and efficient solutions while maintaining a professional and friendly partnership with my clients.',
+              'My name is Oleksii. I’m a Web Developer from Ukraine with over 3 years of experience. I believe in deep dedication to every project, where client satisfaction is the primary goal of my career.',
+              'I specialize in React, Next, JavaScript and TypeScript, with a solid understanding of web hosting and infrastructure. I approach complex challenges with an analytical mindset, ensuring clear and efficient solutions while maintaining a professional and friendly partnership with my clients.',
               'You can rely on me for honest communication and high-quality digital services.',
             ],
           },
@@ -62,7 +62,7 @@ i18n
             thanks: 'Danke schön.',
             intro: [
               'Mein Name ist Oleksii. Ich bin Webentwickler aus der Ukraine und habe mehr als 2 Jahre Berufserfahrung. Ich widme mich jedem Projekt mit voller Hingabe, denn die Zufriedenheit der Kundinnen und Kunden steht im Mittelpunkt meiner beruflichen Laufbahn.',
-              'Ich spezialisiere mich auf HTML, CSS und JavaScript und verfüge über ein solides Verständnis von Webhosting und Infrastruktur. Komplexe Herausforderungen gehe ich analytisch an und sorge für klare, effiziente Lösungen, während ich gleichzeitig eine professionelle und partnerschaftliche Zusammenarbeit mit meinen Kundinnen und Kunden pflege.',
+              'Ich spezialisiere mich auf React, Next, JavaScript and TypeScript und verfüge über ein solides Verständnis von Webhosting und Infrastruktur. Komplexe Herausforderungen gehe ich analytisch an und sorge für klare, effiziente Lösungen, während ich gleichzeitig eine professionelle und partnerschaftliche Zusammenarbeit mit meinen Kundinnen und Kunden pflege.',
               'Sie können sich auf ehrliche und professionelle Dienstleistungen verlassen.',
             ],
           },
